0002cea85c6c91bd67f5fd6102f7a7cc2577e524
maedi <maediprichard@gmail.com>
2026-06-20 00:55:22 +1000

    Use raindeer's boot file

--- config/boot.rb
+++ config/boot.rb
@@ -1,34 +1,6 @@
 # frozen_string_literal: true
 
-# Automatically generated framework-level boot script. You won't typically need to modify this file.
-# You could add an application-level boot script in "app/boot.rb" for example.
+require 'bundler/setup'
+require 'raindeer/boot'
 
-require 'raindeer'
-require 'raindeer/lib/support/config_loader'
-
-env = {
-  host: ENV.fetch('RAIN_HOST', nil),
-  port: ENV.fetch('RAIN_PORT', nil),
-  web_root: ENV.fetch('RAIN_WEB_ROOT', nil),
-  matrix_mode: Rain::ConfigLoader.parse_boolean(ENV.fetch('RAIN_MATRIX', nil)),
-}
-
-config = Rain::ConfigLoader.load('./config/config.yaml', env)
-
-Providers.define('rain.router') do
-  require_relative 'router/router'
-  Rain::Router.new
-end
-
-Providers.define('rain.matrix') do
-  require_relative 'matrix/matrix'
-  Rain::Matrix.new(event_pool: Providers['low.event.pool'])
-end
-
-Providers.define('low.loop') do
-  require 'low_loop'
-  LowLoop.new(config:, router: Providers['rain.router'], renderer: Providers['rain.matrix'])
-end
-
-require 'lowload'
 LowLoad.dirload(File.expand_path('../app', __dir__))
